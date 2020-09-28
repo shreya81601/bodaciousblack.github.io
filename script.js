dc3fa5e04e769a9044602c41799f469b9b643f46
shreya81601 <71819463+shreya81601@users.noreply.github.com>
2020-09-29 02:26:02 +0530

    completed the website with all pages
    even commented out the code

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-/*var img = document.getElementById("myImg");*/
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
-/*document.getElementById("myImg").onclick = */
-
+//function display called by many
 function display()
 {
   modal.style.display = "block";
@@ -15,6 +13,15 @@ function display()
   captionText.innerHTML = this.alt;
 }
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// Add event listeners on the product images
 document.getElementById("myImg1").addEventListener('click', display);
 document.getElementById("myImg2").addEventListener('click', display);
 document.getElementById("myImg3").addEventListener('click', display);
@@ -24,18 +31,3 @@ document.getElementById("myImg6").addEventListener('click', display);
 document.getElementById("myImg7").addEventListener('click', display);
 document.getElementById("myImg8").addEventListener('click', display);
 document.getElementById("myImg9").addEventListener('click', display);
-
-/*document.getElementById("myImg2").onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}*/
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
